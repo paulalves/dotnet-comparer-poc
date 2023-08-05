51c514b8ab2a7a951257f15fd7c14d85f8eb0eca
Paul Alves <paulalves@protonmail.com>
2023-08-05 18:05:31 +0800

    expand hashset tests

--- tests/Comparer.UnitTests/HashSetEqualityComparisonTests.cs
+++ tests/Comparer.UnitTests/HashSetEqualityComparisonTests.cs
@@ -2,6 +2,10 @@ namespace Comparer.UnitTests
 {
   using System.Collections.Generic;
 
+  using Comparer.Extensions;
+
+  using FluentAssertions;
+
   using Xunit;
 
   public class HashSetEqualityComparisonTests : EqualityComparisonTests
@@ -16,6 +20,37 @@ namespace Comparer.UnitTests
     public override void AreNotEqual()
     {
       WhenBothAreNotEqualInternal(new HashSet<object> { 'a', "b" }, new HashSet<object> { 'a', "c" });
+    }
+    
+    [Fact]
+    public void WhenAddDuplicate_ItemsCountShouldBeCorrect_ExcludingDuplicates()
+    {
+      var hashSet = new HashSet<object>(AnyComparer.Default) { 'a', "b", "a", "asdfg" };
+      hashSet.Count.Should().Be(3);
+    }
+    
+    [Fact]
+    public void WhenBothAreEqual_CountainsDuplicates_WithSameItems()
+    {
+      var hashSet1 = new HashSet<object>(AnyComparer.Default) { 'a', "b", "b" };
+      var hashSet2 = new HashSet<object>(AnyComparer.Default) { "a", "b", "a" };
+
+      hashSet1.Count.Should().Be(2);
+      hashSet2.Count.Should().Be(2);
+
+      WhenBothAreEqualInternal(hashSet1, hashSet2);
+    }
+    
+    [Fact]
+    public void WhenBothAreNotEqual_CountainsDuplicates_WithDifferentItems()
+    {
+      var hashSet1 = new HashSet<object>(AnyComparer.Default) { 'a', "b", "a" };
+      var hashSet2 = new HashSet<object>(AnyComparer.Default) { 'a', "c", 'c' };
+
+      hashSet1.Count.Should().Be(2);
+      hashSet2.Count.Should().Be(2);
+      
+      WhenBothAreNotEqualInternal(hashSet1, hashSet2);
     }
   }
 }
